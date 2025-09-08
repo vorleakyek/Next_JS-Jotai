2931d6dc8717b217e7389bc4a5ee9859fcfd4307
Vorleak Yek <vorleakyek@gmail.com>
2025-09-08 16:58:48 -0700

    basic page setup connection

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+'use client'; 
+import { Button } from '@mantine/core';
+import { atom, useAtom } from 'jotai';
+
+const buttonClickedAtom = atom(false);
+
+export default function Home() {
+  const [buttonClicked, setButtonClicked] = useAtom(buttonClickedAtom);
+
+  const handleClick = () => {
+    const toggle = !buttonClicked;
+    setButtonClicked(toggle);
+  };
+
+  return (
+    <div >
+      <Button onClick={handleClick}>Click me</Button>
+      {buttonClicked && <p>Button was clicked!</p>}
+      
+      
+    </div>
+  );
+}
+
+
