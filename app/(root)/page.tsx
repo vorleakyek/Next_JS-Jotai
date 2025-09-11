3b5e52876fe6732bac06ef55d2edd380d23118c3
Vorleak Yek <vorleakyek@gmail.com>
2025-09-10 22:37:44 -0700

    adding github api to retrieve pr info

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,38 @@
 'use client'; 
 import { Button } from '@mantine/core';
 import { atom, useAtom } from 'jotai'; 
+import { Octokit } from "octokit";
 
 
-const buttonClickedAtom = atom(false);
+const prResult = atom("");
 
 export default function Home() {
-  const [buttonClicked, setButtonClicked] = useAtom(buttonClickedAtom);
+  const [prResultValue, setPrResultValue] = useAtom(prResult);
 
-  const handleClick = () => {
-    const toggle = !buttonClicked;
-    setButtonClicked(toggle);
+  const handleClick = async (e) => {
+    e.preventDefault(); 
+    const prResult = await getPrResult();
+    setPrResultValue(prResult);  
+  };
+
+  const getPrResult = async () => {
+    const octokit = new Octokit({ auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN });
+    const response = await octokit.request("GET /repos/chingu-voyages/v57-tier3-team-39",
+      {
+        // org: import.meta.env.VITE_GITHUB_ORG,
+        // team_slug: import.meta.env.VITE_GITHUB_REPO_NAME,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    return response.data;
   };
 
   return (
     <div >
       <Button onClick={handleClick}>Click me</Button>
-      {buttonClicked && <p>Button was clicked!</p>}
-      
-      
+      {prResultValue && <pre>{JSON.stringify(prResultValue, null, 2)}</pre>}
     </div>
   );
 }
